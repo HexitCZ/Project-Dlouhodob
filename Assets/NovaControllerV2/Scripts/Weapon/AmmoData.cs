9da25e0e1e7ffa35472a47ceb18ef6ddb59e8936
HexitCZ <dracek95@gmail.com>
2021-12-27 17:29:37 +0100

    Import FPS Controller

---
+++ Assets/NovaControllerV2/Scripts/Weapon/AmmoData.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoData : MonoBehaviour
+{
+    [System.Serializable]
+    public class AmmoPack
+    {
+        [Tooltip("Bullets currently in a magazine")]
+        public int bullets_in_magazine;
+        [Tooltip("How many bullets can a magazine handle.")]
+        public int magazine_size;
+        [Tooltip("Total bullets left in a magic pouch of bullets for this gun.")]
+        public int bullets_left;
+
+        public override string ToString()
+        {
+            return $"Pack: bullets_in_magazine: {bullets_in_magazine}, magazine_size: {magazine_size}, bullets_left: {bullets_left}";
+        }
+    }
+
+
+    public List<AmmoPack> ammoList;
+
+    public AmmoPack GetAmmoPack(int index)
+    {
+        if (index >= 0 && index < ammoList.Count)
+        {
+            return ammoList[index];
+        }
+        Debug.LogError($"Wrong ammo index: {index}, pack: {ammoList[index]}");
+        return null;
+    }
+
+
+    public void SetAmmoPack(int index, AmmoPack pack)
+    {        
+        if (index >= 0 && index < ammoList.Count)
+        {
+            ammoList[index] = pack;
+            return;
+        }
+        Debug.LogError($"Wrong ammo index: {index}, new pack: {pack}");
+        return;
+        
+    }
+
+}
---
+++ Assets/NovaControllerV2/Scripts/Weapon/WeaponController.cs
@@ -0,0 +1,162 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WeaponController : MonoBehaviour
+{
+
+    public AmmoData ammoData;
+
+    public GameObject weaponRenderer;
+    
+    public WeaponObject weapon;
+
+    [Space]
+    private bool shoot;
+    private double shoot_charge;
+    private RaycastHit hit;
+    private Transform source;
+
+
+    [Space]
+    private bool reload;
+    
+    [Space]
+
+    private AmmoData.AmmoPack ammoPack;
+
+    private MeshFilter meshFilter;
+    private Animator animator;
+    private AudioSource audioSource;
+
+    private bool readyToShoot = true;
+    [SerializeField]
+    private bool shooting;
+    private bool reloading;
+
+    private void Awake()
+    {
+        animator = weaponRenderer.GetComponent<Animator>();
+        meshFilter = weaponRenderer.GetComponent<MeshFilter>();
+        audioSource = GetComponent<AudioSource>();
+        source = weaponRenderer.transform.parent.transform;
+        ammoPack = GetAmmoPack();
+    }
+
+    private void Update()
+    {
+        InputResolver();   
+    }
+    public void SetWeaponObject(WeaponObject newWO)
+    {
+        weapon = newWO;
+    }
+
+    private AmmoData.AmmoPack GetAmmoPack()
+    {
+        return ammoData.GetAmmoPack(weapon.ammoIndex);
+    }
+
+
+    public void InputResolver()
+    {
+        
+        if (shoot && readyToShoot && ammoPack.bullets_in_magazine>0 && !reloading)
+        {
+            if (weapon.fullAuto)
+            {
+                Shoot();
+            }
+            else
+            {
+                Shoot();
+                shoot = false;
+
+            }
+            
+        }
+
+        Debug.Log(ammoPack.magazine_size - ammoPack.bullets_in_magazine + " " + (ammoPack.bullets_left >= (ammoPack.magazine_size - ammoPack.bullets_in_magazine)) + " " + !reloading);
+        
+        if(reload && !reloading && ammoPack.bullets_left >= (ammoPack.magazine_size-ammoPack.bullets_in_magazine))
+        {
+            Reload();
+        }
+        
+    }
+
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        Debug.Log("shoot");
+        
+        Vector3 direction = weaponRenderer.transform.forward;
+        
+
+
+        if (Physics.Raycast(source.position, direction, out hit, weapon.range, weapon.whatCanIHit))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+        else
+        {
+            direction.Scale(Vector3.one * 10);
+        }
+        ammoPack.bullets_in_magazine--;
+        Invoke("ResetShot", weapon.timeBetweenShots);
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        UpdateAmmoData();
+    }
+
+    private void Reload()
+    {
+        Debug.Log("Reloading");
+        reloading = true;
+
+        ammoPack.bullets_left -= ammoPack.magazine_size - ammoPack.bullets_in_magazine;
+        ammoPack.bullets_in_magazine = ammoPack.magazine_size;
+        Invoke("ReloadFinished", weapon.reloadTime);
+    }
+
+    private void ReloadFinished()
+    {
+        reloading = false;
+        Debug.Log("Reloading has been finished");
+        UpdateAmmoData();
+
+    }
+
+
+    private void UpdateAmmoData()
+    {
+        ammoData.SetAmmoPack(weapon.ammoIndex, ammoPack);
+    }
+
+    public void OnShoot(InputAction.CallbackContext press)
+    {
+        if (press.started)
+        {
+            shoot = true;
+        }
+        if (press.canceled)
+        {
+            shoot = false;
+            shoot_charge = press.duration;
+        }
+    }
+
+    public void OnReload(InputAction.CallbackContext press)
+    {
+        reload = press.performed;
+    }
+
+
+
+}
