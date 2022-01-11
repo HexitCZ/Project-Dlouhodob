f6b8a1c5a9190750b4abb1ee94c133faaad81437
HexitCZ <42703633+HexitCZ@users.noreply.github.com>
2022-01-11 11:29:08 +0100

    general update

--- Assets/NovaControllerV2/Scripts/Weapon/WeaponObject.cs
+++ Assets/NovaControllerV2/Scripts/Weapon/WeaponObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Weapon Setting")]
+[CreateAssetMenu(menuName = "Weapon object")]
 public class WeaponObject : ScriptableObject
 {
     [Header("Properties")]
