9ef91e20b483d1ec7c89f0b5755b78fe5d5ec096
HexitCZ <42703633+HexitCZ@users.noreply.github.com>
2022-02-21 09:00:23 +0100

    sadfgvbn

---
+++ Assets/Scripts/Tomas/Gameplay/AI/AI_Walker.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Walker : AI_Base
+{
+    public bool removeHealth;
+    public float slowDownSpeed;
+    public float slowDownLength;
+    private float startSpeed;
+    private bool isSlowed;
+
+    private new void Start()
+    {
+        base.Start();
+        preUpdateAction += RemoveHealth;
+        startSpeed = navmesh.speed;
+    }
+
+    private void RemoveHealth()
+    {
+        if (removeHealth)
+        {
+            health--;
+            removeHealth = !removeHealth;
+        }
+    }
+
+    protected override void SetDestination(Transform destination)
+    {
+        base.SetDestination(destination);
+    }
+
+    protected override bool CheckHealth()
+    {
+        Debug.Log("CheckHealth");
+
+        if (health > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public override void GetHit()
+    {
+        base.GetHit();
+        TrySlowDown();
+    }
+
+    private void TrySlowDown()
+    {
+        if (!isSlowed)
+        {
+            isSlowed = true;
+            Invoke("ResetSpeed", slowDownLength);
+            navmesh.speed = slowDownSpeed;
+        }
+    }
+
+    private void ResetSpeed()
+    {
+        isSlowed = false;
+        navmesh.speed = startSpeed;
+    }
+
+
+
+    protected override bool CheckVisibility()
+    {
+        //Debug.Log("CheckVisibility");
+        return true;
+    }
+
+    protected override bool CheckRange()
+    {
+        //Debug.Log("CheckRange");
+        return true;
+    }
+
+    protected override void Attack()
+    {
+        //Debug.Log("Attack");
+    }
+
+    protected override void Death()
+    {
+
+        Debug.Log("Death");
+        gameObject.SetActive(false);
+
+
+    }
+}
