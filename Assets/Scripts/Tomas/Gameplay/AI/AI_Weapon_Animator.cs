747f5f29838ef677d9e624c3f441748caa039732
HexitCZ <42703633+HexitCZ@users.noreply.github.com>
2022-03-08 23:17:32 +0100

    HUUUUUUU

--- Assets/Scripts/Tomas/Gameplay/AI/AI_Weapon_Animator.cs
+++ Assets/Scripts/Tomas/Gameplay/AI/AI_Weapon_Animator.cs
@@ -14,9 +14,11 @@ public class AI_Weapon_Animator : MonoBehaviour
 
     void Update()
     {
-        yRot = Quaternion.Euler(Vector3.Scale(Ymask, (Quaternion.LookRotation((this.transform.position - target.transform.position).normalized, Vector3.up).eulerAngles)));
-        xRot = Quaternion.Euler(Vector3.Scale(Xmask, (Quaternion.LookRotation((this.transform.position - target.transform.position).normalized, Vector3.up).eulerAngles)));
-        Y.transform.rotation = Quaternion.Slerp(Y.transform.rotation, yRot, 0.025f);
+        Vector3 playerDirection = (target.transform.position - this.transform.position).normalized;
+        Quaternion lookRot = Quaternion.LookRotation(playerDirection, Vector3.up);
+        yRot = lookRot;
+        xRot = lookRot;
+        //Y.transform.rotation = Quaternion.Slerp(Y.transform.rotation, yRot, 0.025f);
         X.transform.rotation = Quaternion.Slerp(X.transform.rotation, xRot, 0.025f);
         
         
