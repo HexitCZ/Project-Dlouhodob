9316236a9cce1a7ca9a3590af5f85686cfd5389b
LukGub <czluk45@gmail.com>
2022-03-26 12:06:44 +0100

    hihi

--- Assets/Scripts/Lukas/Menu/UIOptionsManager.cs
+++ Assets/Scripts/Lukas/Menu/UIOptionsManager.cs
@@ -31,6 +31,11 @@ public class UIOptionsManager : MonoBehaviour
 
     private float setVolume;
 
+    [SerializeField]
+    [Space]
+    [Header("Background image reference")]
+    private Transform background;
+
     public TMP_Dropdown resolutionDropdown;
     public TMP_Dropdown qualityDropdown;   
 
@@ -40,15 +45,56 @@ public class UIOptionsManager : MonoBehaviour
 
     void Start()
     {
-        isFullscreen = false;
+
+        isFullscreen = Screen.fullScreen;
         currentVolume = 0f;
         mainMenuScene = "Menu";
         
+    }
+    
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(2);
+    }
+
+    public void Awake()
+    {
+
+        /*UnvailGradient();
+        StartCoroutine(Wait());*/
+
+    }
+
+    public void OnDestroy()
+    {
+        
+        /*CoverGradient();
+        StartCoroutine(Wait());*/
+
+
     }
 
     void Update()
     {
         currentVolume = AudioListener.volume;
+
+    }
+
+    public void UnvailGradient()
+    {
+        for (float f = 0.01f; f <= 1.0f; f++)
+        {
+            background.GetComponent<Image>().fillAmount += f;
+        }
+    }
+
+    public void CoverGradient()
+    {
+        for (float f = 1.0f; f >= 0.0f; f--)
+        {
+            background.GetComponent<Image>().fillAmount -= f;
+        }
     }
 
     private float GetVolumeFromSlider()
@@ -67,14 +113,25 @@ public class UIOptionsManager : MonoBehaviour
 
     private void SetFullscreen()
     {
+        fullscreenButton.GetChild(0).GetComponent<TextMeshProUGUI>().text = "windowed";
         Screen.fullScreen = true;
+        TextMeshProUGUI fullscreen_text = fullscreenButton.GetChild(0).GetComponent<TextMeshProUGUI>();
+        fullscreen_text.text = "Windowed";
+        fullscreen_text.fontSize = 45;
+        Debug.Log("fullscreen");
     }
     
     private void SetWindowed()
     {
+
+        TextMeshProUGUI fullscreen_text = fullscreenButton.GetChild(0).GetComponent<TextMeshProUGUI>();
+        fullscreen_text.text = "Full\nscreen";
+        fullscreen_text.fontSize = 52.7f;
         Screen.fullScreen = false;
+        Debug.Log("windowed");
+
     }
-    
+
 
     public void OnSliderDrag()
     {
@@ -92,10 +149,25 @@ public class UIOptionsManager : MonoBehaviour
 
         isFullscreen = !isFullscreen;
 
+        if (isFullscreen)
+        {
+            SetFullscreen();
+        }
+        else
+        {
+            SetWindowed();
+        }
     }
 
     public void OnGraphicPresetChange()
     {
-        Debug.Log(QualitySettings.currentLevel);
+        int qualityIndex = qualityDropdown.value;
+        qualityIndex += 1;
+        ChangeGraphicsPreset(qualityIndex);
+    }
+
+    private void ChangeGraphicsPreset(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
     }
 }
---
+++ Assets/Scripts/Lukas/Menu/fourWayGradientScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fourWayGradientScript : MonoBehaviour
+{
+    [SerializeField]
+    [Space]
+    [Header("Top left")]
+    private Color t_l;
+    
+    [SerializeField]
+    [Space]
+    [Header("Top right")]
+    private Color t_r;
+    
+    [SerializeField]
+    [Space]
+    [Header("Bottom left")]
+    private Color b_l;
+
+    [SerializeField]
+    [Space]
+    [Header("Bottom right")]
+    private Color b_r;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
