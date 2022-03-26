6364da23b11bc76c6f1cb011a29bf813595cbf2e
HexitCZ <42703633+HexitCZ@users.noreply.github.com>
2022-03-26 12:04:32 +0100

    Focus on maturita engliseh

--- Assets/Scripts/Tomas/Menu/PlayerUI.cs
+++ Assets/Scripts/Tomas/Menu/PlayerUI.cs
@@ -1,14 +1,28 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.InputSystem;
 
 public class PlayerUI : MonoBehaviour
 {
     public GameObject upgrade;
     public GameObject hud;
+    public float fpsUpdateCooldown;
+    public TMP_Text fps_text;
 
-    
+    private void Start()
+    {
+        InvokeRepeating("UpdateFPS",0.0f , fpsUpdateCooldown);
+    }
+
+    private void UpdateFPS()
+    {
+        fps_text.text = "FPS: " + Math.Round(1 / Time.deltaTime, 1);
+    }
+
+
     public void OnUpgradeClick(InputAction.CallbackContext inp)
     {
         if (inp.performed)
