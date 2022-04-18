55eb03c5718497f63bf9edb7192a552701291e2c
HexitCZ <42703633+HexitCZ@users.noreply.github.com>
2022-04-18 20:43:41 +0200

    colcititit

--- Assets/Scripts/Tomas/Gameplay/Environments/FanRotation.cs
+++ Assets/Scripts/Tomas/Gameplay/Environments/FanRotation.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class FanRotation : MonoBehaviour
 {
     public float rotationSpeed;
+    public float maxFluctuation;
 
     //Makes environments nicer with random fan rotation
 
     private void Update()
     {
-        float fluctuation = Mathf.Lerp(1.0f, Random.Range(0.1f,3f), 1f);
+        float fluctuation = Mathf.Lerp(1.0f, Random.Range(0.1f, maxFluctuation), 1f);
         transform.Rotate(0, rotationSpeed * Time.deltaTime * fluctuation, 0);
     }
 }
---
+++ Assets/cloc_it_by_file.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+echo ------------------Tomas Krejcik------------------
+cloc G:\Programs\Unity\UnityProjects\Project-Dlouhodob-again\Project-Dlouhodob\Assets\Scripts\Tomas --by-file
+echo ------------------Lukas Gubala------------------
+cloc G:\Programs\Unity\UnityProjects\Project-Dlouhodob-again\Project-Dlouhodob\Assets\Scripts\Lukas --by-file
+
+
+
+
+echo Press any key to end this program
+echo (or just close this window via exit button)
+pause
+exit
